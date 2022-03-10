39c8bef401c5d3d03589b8be69ce160f66f34803
Alexander Huang <alexanderhuangen@live.se>
2022-03-10 13:37:34 +0100

    huge changes

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 from tensorflow.keras.preprocessing.image import smart_resize, img_to_array
-from config import host, img_size, model_path
+from ml.config import host, img_size, model_path
 from utils import im_2_b64
 
 
